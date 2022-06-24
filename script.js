7de314e4dea7bc906a75193f50a14abd6cb9c404
tissyane <tissyane@gmail.com>
2022-06-23 23:45:24 -0300

    HTML e CSS finalizados

---
+++ script.js
@@ -0,0 +1,11 @@
+function showParticipants() {
+    let activar = document.querySelector(".participants")
+    activar.classList.remove("escondido")
+
+}
+
+function hideParticipants() {
+    let desactivar = document.querySelector(".participants")
+    desactivar.classList.add("escondido")
+
+}
