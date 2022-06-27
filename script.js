a2880f4cd812e8f94db34c9c129651c80fb43cd7
tissyane <tissyane@gmail.com>
2022-06-27 13:08:42 -0300

    Tela de entrada

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let login;
+
 function showParticipants() {
   let activate = document.querySelector(".participants");
   activate.classList.remove("hidden");
@@ -8,19 +10,17 @@ function hideParticipants() {
   disable.classList.add("hidden");
 }
 
-nameLogin();
-function nameLogin() {
-  login = prompt("Escolha um nome de usuário!");
-  person = {
-    name: login,
-  };
+
+function enterName() {
+  login = document.querySelector(".entrada input").value;
+  
   start();
 }
 
 function start() {
   let promise = axios.post(
     "https://mock-api.driven.com.br/api/v6/uol/participants",
-    person
+    {name:login}
   );
   promise.then(enterTheRoom);
   promise.catch(cantEnter);
@@ -30,48 +30,47 @@ function cantEnter(erro) {
   if (erro.response.status === 400) {
     alert("Ops, alguém já escolheu esse. Escolhe outro aí.");
 
-    nameLogin();
   }
 }
 
 function enterTheRoom() {
+  let init = document.querySelector(".principal");
+  init.classList.remove("escondido");
+
   const promise = axios.get(
     "https://mock-api.driven.com.br/api/v6/uol/messages"
   );
   promise.then(showMessages);
+
+  keepConected();
 }
 
 function showMessages(msg) {
-  let messages = document.querySelector(".messages");
-  messages.innerHTML = "";
+  let plotMessages = document.querySelector(".messages");
+  plotMessages.innerHTML = "";
   for (let i = 0; i < msg.data.length; i++) {
     switch (msg.data[i].type) {
       case "status":
-        messages.innerHTML += `
-              <div class="mensagens">
-                  <div class="status">
-                      <p><span class="msg_time">(${msg.data[i].time})</span> <strong> ${msg.data[i].from}</strong> ${msg.data[i].text}</p>
-                  </div>
-              </div>
-              `;
+        plotMessages.innerHTML += `      
+          <div class="status">
+            <p><span class="msg_time">(${msg.data[i].time})</span> <strong> ${msg.data[i].from}</strong> ${msg.data[i].text}</p>
+          </div>
+        `;
         break;
       case "message":
-        messages.innerHTML += `
-              <div class="mensagens">
-                  <div class="msg_to_all">
-                      <p><span class="msg_time">(${msg.data[i].time})</span> <strong> ${msg.data[i].from} </strong> para <strong>${msg.data[i].to}</strong>: ${msg.data[i].text}</p>
-              </div>
-              `;
+        plotMessages.innerHTML += `
+          <div class="msg_to_all">
+              <p><span class="msg_time">(${msg.data[i].time})</span> <strong> ${msg.data[i].from} </strong> para <strong>${msg.data[i].to}</strong>: ${msg.data[i].text}</p>
+          </div>
+        `;
         break;
       case "private_message":
         if (login === msg.data[i].to) {
-          messages.innerHTML += `
-                  <div class="mensagens">
-                      <div class="msg_private">
-                          <p><span class="msg_time">(${msg.data[i].time})</span> <strong> ${msg.data[i].from}</strong> reservadamente para <strong>${msg.data[i].to}</strong>: ${msg.data[i].text}</p>
-                      </div>
-                  </div>
-                  `;
+          plotMessages.innerHTML += `
+            <div class="msg_private">
+              <p><span class="msg_time">(${msg.data[i].time})</span> <strong> ${msg.data[i].from}</strong> reservadamente para <strong>${msg.data[i].to}</strong>: ${msg.data[i].text}</p>
+            </div>
+            `;
         }
         break;
       default:
@@ -79,21 +78,20 @@ function showMessages(msg) {
     }
   }
   scroll();
-  
 }
 
 function scroll() {
-  const chat = document.querySelector(".messages")
-  const lastmsg = chat.lastElementChild
+  const chat = document.querySelector(".messages");
+  const lastmsg = chat.lastElementChild;
   lastmsg.scrollIntoView();
 }
 
 function keepConected() {
   const promise = axios.post(
     "https://mock-api.driven.com.br/api/v6/uol/status",
-    person
+    {name:login}
   );
-  promise.then();
+  promise.then(setTimeout(keepConected, 5000));
   promise.catch(sair);
 }
 
@@ -116,7 +114,7 @@ function sendMessage() {
 
   text_msg = msgInput.value;
 
-  const novaMsg = {
+  const newMsg = {
     from: login,
     to: "Todos",
     text: text_msg,
@@ -125,7 +123,7 @@ function sendMessage() {
 
   const promise = axios.post(
     `https://mock-api.driven.com.br/api/v6/uol/messages`,
-    novaMsg
+    newMsg
   );
   promise.then(updateMessages);
   promise.catch(refreshPage);
@@ -133,17 +131,17 @@ function sendMessage() {
   msgInput.value = "";
 }
 
-function refreshPage(){
+document.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    const btn = document.querySelector(".send_button");
+    e.preventDefault()
+    btn.click();
+  }
+});
+
+function refreshPage() {
   window.location.reload();
 }
 
-document.addEventListener("keypress", function(e) {
 
-  if(e.key === "Enter") {
-  const btn = document.querySelector(".send_button");
-  btn.click();
-  }
-})
-
-const online = setInterval(keepConected, 5000);
 const atualizado = setInterval(updateMessages, 3000);
